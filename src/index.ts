c281914765188556248e54057df0e6577f319196
jfrazx <staringblind@gmail.com>
2024-07-15 07:56:29 -0700

    feat(esm): build for esm and commonjs

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,18 @@
  * @param {T} klass
  * @returns T
  */
-export const Abstract = <T extends Function>(klass: T) => {
+export function Abstract<T extends Function>(klass: T) {
   return new Proxy(klass, new AbstractHandler());
-};
+}
 
 class AbstractHandler<T extends Function> implements ProxyHandler<T> {
   construct(target: T, args: any[], newTarget: Function) {
-    if (this.shouldThrow(target, newTarget)) {
-      this.throwError(target);
+    if (target.prototype.constructor === newTarget.prototype.constructor) {
+      throw new Error(
+        `'${target.name}' is an Abstract class and cannot be instantiated directly`
+      );
     }
 
     return Reflect.construct(target, args, newTarget);
-  }
-
-  private shouldThrow(target: T, newTarget: Function) {
-    return target.prototype.constructor === newTarget.prototype.constructor;
-  }
-
-  private throwError(target: T): never {
-    throw new Error(
-      `'${target.name}' is an Abstract class and cannot be instantiated directly`,
-    );
   }
 }
